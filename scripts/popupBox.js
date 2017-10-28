7fb3c7e075511ad7632f62bed810986938cf67ce
Mohammad Saif Shaikh <saifshaikh48@gmail.com>
2017-10-28 17:11:04 -0400

    UI files

---
+++ scripts/popupBox.js
@@ -0,0 +1,49 @@
+
+var moveLeft = 0;
+var moveDown = 0;
+$('a.popper').hover(function (e) {
+
+    var target = '#' + ($(this).attr('data-popbox'));
+    $(target).show();
+    moveLeft = $(this).outerWidth();
+    moveDown = ($(target).outerHeight() / 2);
+}, function () {
+    var target = '#' + ($(this).attr('data-popbox'));
+    if (!($("a.popper").hasClass("show"))) {
+        $(target).hide();
+    }
+});
+
+$('a.popper').mousemove(function (e) {
+    var target = '#' + ($(this).attr('data-popbox'));
+
+    leftD = e.pageX + parseInt(moveLeft);
+    maxRight = leftD + $(target).outerWidth();
+    windowLeft = $(window).width() - 10;
+    windowRight = 0;
+    maxLeft = e.pageX - (parseInt(moveLeft) + $(target).outerWidth() + 10);
+
+    if (maxRight > windowLeft && maxLeft > windowRight) {
+        leftD = maxLeft;
+    }
+
+    topD = e.pageY - parseInt(moveDown);
+    maxBottom = parseInt(e.pageY + parseInt(moveDown) + 10);
+    windowBottom = parseInt(parseInt($(document).scrollTop()) + parseInt($(window).height()));
+    maxTop = topD;
+    windowTop = parseInt($(document).scrollTop());
+    if (maxBottom > windowBottom) {
+        topD = windowBottom - $(target).outerHeight() - 10;
+    } else if (maxTop < windowTop) {
+        topD = windowTop + 10;
+    }
+
+    $(target).css('top', topD).css('left', leftD);
+});
+$('a.popper').click(function (e) {
+    var target = '#' + ($(this).attr('data-popbox'));
+    if (!($(this).hasClass("show"))) {
+        $(target).show();
+    }
+    $(this).toggleClass("show");
+});
