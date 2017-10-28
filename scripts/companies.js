79df8506ae0e1ac26a5cad353a03b60900e82605
JMichaelYang <jaewonmichael@gmail.com>
2017-10-28 16:59:42 -0400

    Replacing shit

--- scripts/companies.js
+++ scripts/companies.js
@@ -1,27 +1,43 @@
 // Unique ID for the className.
 var MOUSE_VISITED_CLASSNAME = 'crx_mouse_visited';
 
+var re = /company/i;
+var walker = document.createTreeWalker(
+    document.body,
+    NodeFilter.SHOW_TEXT,
+    function (node) {
+        var matches = node.textContent.match(re);
+
+        if (matches) {
+            return NodeFilter.FILTER_ACCEPT;
+        } else {
+            return NodeFilter.FILTER_SKIP;
+        }
+    },
+    false);
+
+var nodes = [];
+
+while (walker.nextNode()) {
+    nodes.push(walker.currentNode);
+}
+
+for (var i = 0; node = nodes[i]; i++) {
+    node.parentNode.innerHTML = node.parentNode.innerHTML.replace(re, "@$1")
+}
+
+/*
 // Previous dom, that we want to track, so we can remove the previous styling.
 var prevDOM = null;
 
-// Mouse listener for any move event on the current document.
-document.addEventListener('mousemove', function (e) {
-  var srcElement = e.srcElement;
+// Get all elements on the page
+var items = document.getElementsByTagName("*");
 
-  // Lets check if our underlying element is a DIV.
-  if (srcElement.childNodes[0].textContent.includes("company")) {
-
-    // For NPE checking, we check safely. We need to remove the class name
-    // Since we will be styling the new one after.
-    if (prevDOM != null) {
-      prevDOM.classList.remove(MOUSE_VISITED_CLASSNAME);
+// Loop through all elements in the document
+for (var i = items.length; i--;) {
+    if (items[i].childNodes[0].textContent.includes("company")) {
+        // Style any element that contains the word
+        items[i].classList.add(MOUSE_VISITED_CLASSNAME);
     }
-
-    // Add a visited class name to the element. So we can style it.
-    srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
-
-    // The current element is now the previous. So we can remove the class
-    // during the next iteration.
-    prevDOM = srcElement;
-  }
-}, false);
+    console.log(items[i].firstElementChild.textContent);
+}*/
